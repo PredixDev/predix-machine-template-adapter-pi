b6969a9372d6b331e09b06535d2de6dffc63e987
turnerth <turnerth@ge.com>
2017-03-13 17:59:26 -0700

    Update quickstart.sh

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -110,7 +110,7 @@ echo "MACHINE_VERSION : $MACHINE_VERSION"
 echo "PREDIX_MACHINE_HOME : $PREDIX_MACHINE_HOME"
 cd "$CURRENT_DIR"
 
-__print_center "Build and setup the Predix Machine Adapter for Intel Device" "#"
+__print_center "Build and setup the Predix Machine Adapter for Device" "#"
 
 __echo_run cp "$CURRENT_DIR/config/com.ge.predix.solsvc.workshop.adapter.config" "$PREDIX_MACHINE_HOME/configuration/machine"
 __echo_run cp "$CURRENT_DIR/config/com.ge.predix.workshop.nodeconfig.json" "$PREDIX_MACHINE_HOME/configuration/machine"
